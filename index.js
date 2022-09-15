9b94ed3b406c9f725fd20f60b9cae55344d44e77
Bob <bob@mymusclechef.com>
2022-09-15 16:51:39 +1000

    add for iphone 14 pro support

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-import { Dimensions, Platform, StatusBar } from "react-native";
+import { Dimensions, Platform, StatusBar } from 'react-native';
 
 const STATUSBAR_DEFAULT_HEIGHT = 20;
 const STATUSBAR_X_HEIGHT = 44;
 const STATUSBAR_IP12_HEIGHT = 47;
 const STATUSBAR_IP12MAX_HEIGHT = 47;
+const STATUSBAR_IP14PRO_HEIGHT = 54;
+const STATUSBAR_IP14PROMAX_HEIGHT = 54;
 
 const X_WIDTH = 375;
 const X_HEIGHT = 812;
@@ -14,19 +16,27 @@ const XSMAX_HEIGHT = 896;
 const IP12_WIDTH = 390;
 const IP12_HEIGHT = 844;
 
+const IP14PRO_WIDTH = 393;
+const IP14PRO_HEIGHT = 852;
+
 const IP12MAX_WIDTH = 428;
 const IP12MAX_HEIGHT = 926;
 
-const { height: W_HEIGHT, width: W_WIDTH } = Dimensions.get("window");
+const IP14PROMAX_WIDTH = 430;
+const IP14PROMAX_HEIGHT = 932;
+
+const { height: W_HEIGHT, width: W_WIDTH } = Dimensions.get('window');
 
 let statusBarHeight = STATUSBAR_DEFAULT_HEIGHT;
 let isIPhoneX_v = false;
 let isIPhoneXMax_v = false;
 let isIPhone12_v = false;
 let isIPhone12Max_v = false;
+let isIPhone14Pro_v = false;
+let isIPhone14ProMax_v = false;
 let isIPhoneWithMonobrow_v = false;
 
-if (Platform.OS === "ios" && !Platform.isPad && !Platform.isTVOS) {
+if (Platform.OS === 'ios' && !Platform.isPad && !Platform.isTVOS) {
   if (W_WIDTH === X_WIDTH && W_HEIGHT === X_HEIGHT) {
     isIPhoneWithMonobrow_v = true;
     isIPhoneX_v = true;
@@ -43,6 +53,14 @@ if (Platform.OS === "ios" && !Platform.isPad && !Platform.isTVOS) {
     isIPhoneWithMonobrow_v = true;
     isIPhone12Max_v = true;
     statusBarHeight = STATUSBAR_IP12MAX_HEIGHT;
+  } else if (W_WIDTH === IP14PRO_WIDTH && W_HEIGHT === IP14PRO_HEIGHT) {
+    isIPhoneWithMonobrow_v = true;
+    isIPhone14Pro_v = true;
+    statusBarHeight = STATUSBAR_IP14PRO_HEIGHT;
+  } else if (W_WIDTH === IP14PROMAX_WIDTH && W_HEIGHT === IP14PROMAX_HEIGHT) {
+    isIPhoneWithMonobrow_v = true;
+    isIPhone14ProMax_v = true;
+    statusBarHeight = STATUSBAR_IP14PROMAX_HEIGHT;
   }
 }
 
@@ -50,6 +68,8 @@ export const isIPhoneX = () => isIPhoneX_v;
 export const isIPhoneXMax = () => isIPhoneXMax_v;
 export const isIPhone12 = () => isIPhone12_v;
 export const isIPhone12Max = () => isIPhone12Max_v;
+export const isIPhone14Pro = () => isIPhone14Pro_v;
+export const isIPhone14ProMax = () => isIPhone14ProMax_v;
 export const isIPhoneWithMonobrow = () => isIPhoneWithMonobrow_v;
 
 const getExpoRoot = () => global.Expo || global.__expo || global.__exponent;
